aad3413386c02606b39a749efdcc8a882158f313
Sengchanasa <joshualuangasa@gmail.com>
2021-03-21 16:12:10 -0400

    Importing Covid Count project onto github

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
 # Covid_Count
+[![Jovo Framework](https://www.jovo.tech/img/github-logo.png)](https://www.jovo.tech)
+This template contains a Jovo Voice App displaying the number of days since covid, The user asking utterances like "how long". This is the template for the `jovo new` command.
+
+## Quick Start
+
+To use the Jovo covid_count, you'll need the Jovo CLI. You can install it globally with NPM.
+
+```sh
+$ npm install -g jovo-cli
+```
+
+After successfully installing the Jovo CLI, you can install the template using one of the following commands:
+
+```sh
+$ jovo new Covid_Count
+```
+
+Change your working directory into your newly created project directory and run your voice app:
+
+```sh
+# Change working directory to your previously specified directory.
+$ cd <directory>
+
+# Install dependencies.
+$ npm install
+
+# Run voice app, optionally with a --watch flag to restart on code changes.
+$ jovo run [-w]
+```
+
+> Read more about `jovo run` [here](https://www.jovo.tech/marketplace/jovo-cli#jovo-run).
+
+If you now go to the [Jovo Debugger](https://www.jovo.tech/marketplace/jovo-plugin-debugger) by pressing `.` or clicking on the webhook url in the terminal, you can test your voice application right away.
+
+
+## Resources
+-   [Documentation]()
+## About Developer
+
+Joshua Luangasa is a software developer in hopes of making an impact to the community through products using jovo framework for voice, including platforms like Alexa, Google Assistant.
+
+### Socials
+
+-   [linkedin](https://www.linkedin.com/in/joshua-luangasa/)
+-   [Twitter](https://twitter.com/sengchanasa)
+
---
+++ src/app.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const { App } = require('jovo-framework');
+const { Alexa } = require('jovo-platform-alexa');
+const { GoogleAssistant } = require('jovo-platform-googleassistant');
+const { JovoDebugger } = require('jovo-plugin-debugger');
+const { FileDb } = require('jovo-db-filedb');
+
+// ------------------------------------------------------------------
+// APP INITIALIZATION
+// ------------------------------------------------------------------
+
+const app = new App();
+
+app.use(
+  new Alexa(),
+  new GoogleAssistant(),
+  new JovoDebugger(),
+  new FileDb()
+);
+
+// ------------------------------------------------------------------
+// APP LOGIC
+// ------------------------------------------------------------------
+
+app.setHandler({
+  LAUNCH() {
+    return this.toIntent('DaysSinceCovidIntent');
+  },
+
+  DaysSinceCovidIntent() {
+    let end_date = new Date()
+    let start_date = new Date("03/11/2020")
+    let length_time = end_date.getTime() - start_date.getTime()
+    let days = (length_time / (1000 * 3600 * 24)) + 1
+
+    this.$speech.addText('Its been ' + Math.floor(days) + ' days since the start of the pandemic on March 11, 2020.')
+    this.tell(this.$speech)
+  },
+
+  WelcomeIntent() {
+
+    let welcomeMessages = 
+    ["Hello, you are using 'days since covid-19'. ",
+     "Thank you for using this application.",
+     "This application's functionality calculates how many days since covid",
+    ]
+    this.$speech.addText(welcomeMessages)
+    this.ask(this.$speech);
+  },
+
+  MyNameIsIntent() {
+    this.tell('Hey ' + this.$inputs.name.value + ', nice to meet you!');
+  },
+});
+
+module.exports = { app };
